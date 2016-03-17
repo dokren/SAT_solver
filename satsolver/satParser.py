725b57d2b2a06605b045ad3ec14b61f5fc65ba8c
dokren <dk.nexus@gmail.com>
2016-03-17 19:49:01 +0100

    basic SAT solver in working condition(kinda fast)

--- satsolver/satParser.py
+++ satsolver/satParser.py
@@ -1,37 +1,29 @@
-from boolean import *
-
 def parseInput(file):
     file = open(file, 'r')
     formula = list()
     literals = dict()
-    nbvar = 0
     nbclauses = 0
     for line in file:
         line = line.strip()
         if line.startswith('c'):
             continue
         if line.startswith('p cnf'):
-            split = line.split(' ')
+            split = line.split()
             if len(split) != 4:
                 raise Exception('File is not in dimacs format!')
             nbvar = int(split[2])
             nbclauses = int(split[3])
             for i in range(1, nbvar+1):
-                literals[str(i)] = None
+                literals[i] = None
         else:
             if nbclauses == 0 or not line.endswith('0'):
                 raise Exception('File is not in dimacs format!')
-            clause = Or(map(checkNot, filter(removeEmpty, line.split(' ')[:-1])))
+            clause = list(map(int, filter(removeEmpty, line.split()[:-1])))
             formula.append(clause)
-            nbclauses-=1
-    return (And(formula), literals)
-    
+            nbclauses -= 1
+    file.close()
+    return formula, literals
 
-def checkNot(x):
-    x = x.strip()
-    if x.startswith('-'):
-        return Not(x[1:])
-    return x
 
 def removeEmpty(x):
     if x == '':
--- satsolver/satSolver.py
+++ satsolver/satSolver.py
@@ -1,57 +1,62 @@
-from boolean import *
 from satParser import *
+from copy import deepcopy
+
 
 def solve(file):
     (formula, literals) = parseInput(file)
     print('FORMULA: ' + str(formula))
-    print(dpll(formula, literals))
-    
+    print('LITERALS: ' + str(literals))
+
+    (solution, satisfiable) = dpll(formula, literals)
+
+    print('IS SATISFIABLE: ' + str(satisfiable))
+    print('END SOLUTION: ' + str(solution))
+
 
 def dpll(formula, literals):
-    print('FORMULA:')
-    print(formula)
-    formula = formula.simplify()
-    print('FORMULA - SIMPLIFIED:')
-    print(formula)
-    if formula == T:
-        return literals
-    if formula == F:
-        return None
-    if isinstance(formula, Literal):
-        literals[formula.lit] = True
-        return literals
-    if isinstance(formula, Not):
-        literals[formula.term.lit] = False
-        return literals
-    if isinstance(formula, Or):
-        item = next(iter(formula.lst))
-        if isinstance(item, Literal):
-            literals[item.lit] = True
-            return literals
-        if isinstance(item, Not):
-            literals[item.term.lit] = False
-            return literals
-    ## remove unit clauses and set them the required value
-    for clause in formula.lst:
-        if isinstance(clause, Literal):
-            literals[clause.lit] = True
-            formula = formula.removeClause(clause)
-        if isinstance(clause, Not):
-            literals[clause.term.lit] = False
-            formula = formula.removeClause(clause)
+    clearUnits(formula, literals)
+    if not formula:
+        return literals, True
+    elif [] in formula:
+        return literals, False
 
-    if formula == T:
-        return literals
-    if formula == F:
-        return None
-    
-    
-    for (k,v) in literals.items():
+    for (k, v) in literals.items():
         if v == None:
-            newLiterals = dpll(formula.addClause(Literal(k)), dict(literals))
-            if not newLiterals is None:
-                return newLiterals
-            else:
-                return dpll(formula.addClause(Not(Literal(k))), dict(literals))
-    
-solve('dimacs/test.txt')
+            newLiterals = dict(literals)
+            newFormula = deepcopy(formula)
+            newFormula.append([k])
+            (solution, satisfiable) = dpll(newFormula, newLiterals)
+            if satisfiable:
+                return solution, satisfiable
+            newLiterals = dict(literals)
+            newFormula = deepcopy(formula)
+            newFormula.append([-k])
+            return dpll(newFormula, newLiterals)
+
+
+def clearUnits(formula, literals):
+    i = 0
+    while i < len(formula):
+        clause = formula[i]
+        if len(clause) == 1:
+            literals[abs(clause[0])] = True if clause[0] > 0 else False
+            j = 0
+            while j < len(formula):
+                next = False
+                tempClause = formula[j]
+                if set(tempClause) == set(clause):
+                    formula.remove(tempClause)
+                    next = True
+                elif clause[0] in tempClause:
+                    formula.remove(tempClause)
+                    next = True
+                elif -clause[0] in tempClause:
+                    tempClause.remove(-clause[0])
+                    next = True
+                j = j if next else j + 1
+            i = 0
+            continue
+        i += 1
+
+
+solve('dimacs/sudoku2.txt')
