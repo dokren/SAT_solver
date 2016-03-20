8a6b476a1379b548ee9bbeaefe5a60ec41d575d2
rokkolesa <rok.kolesa@gmail.com>
2016-03-20 20:53:25 +0100

    Added small optimizations for the main algorithm

---
+++ satsolver/satParserOpt.py
@@ -0,0 +1,57 @@
+from array import array
+
+def parseInput(file):
+    file = open(file, 'r')
+    formula = list()
+    nbclauses = 0
+    for line in file:
+        line = line.strip()
+        if line.startswith('c'):
+            continue
+        if line.startswith('p cnf'):
+            split = line.split()
+            if len(split) != 4:
+                raise Exception('File is not in dimacs format!')
+            nbvar = int(split[2])
+            nbclauses = int(split[3])
+            # values of literals: -1: false, 0: n/a, 1: true
+            literals = array('i', (0,)*nbvar)
+            # how many times does a literal occur in formula
+            occurrences = array('i', (0,)*nbvar)
+            # sums occurrences of a literal i in original formula: +1 for True, -1 for False
+            purity = array('i', (0,)*nbvar)
+        else:
+            try:
+                if nbclauses == 0 or not line.endswith('0'):
+                    raise Exception('File is not in dimacs format!')
+                clause = array('i', map(int, filter(removeEmpty, line.split()[:-1])))
+                for i in clause:
+                    occurrences[abs(i)-1] += 1
+                    purity[abs(i)-1] += int(i/abs(i))
+                formula.append(clause)
+                nbclauses -= 1
+            except NameError:
+                raise Exception('File is not in dimacs format!')
+    
+    file.close()
+    return (formula, literals, occurrences, purity)
+
+
+def removeEmpty(x):
+    if x == '':
+        return False
+    return True
+
+def writeOutput(file, literals, satisfiable):
+    file = open(file, 'w')
+    if not satisfiable:
+        file.write('NOT SATISFIABLE!')
+    else:
+        for i in range(1, len(literals)+1):
+            if literals[i-1] > 0:
+                file.write(str(i) + ' ')
+            else:
+                file.write('-' + str(i) + ' ')
+
+    file.close()
+
---
+++ satsolver/satSolverOpt.py
@@ -0,0 +1,67 @@
+from satParserOpt import *
+from copy import deepcopy
+
+
+def solve(inputFile, outputFile):
+    print('Parsing input file...')
+    (formula, literals, occurrences, purity) = parseInput(inputFile)
+    print('Removing pure literals...')
+    removePureLiterals(formula, literals, occurrences, purity)
+    print('Running main DPLL algorithm...')
+    (solution, satisfiable) = dpll(formula, literals, occurrences)
+    print('Found solution, writing on output file...')
+    writeOutput(outputFile, solution, satisfiable)
+
+def removePureLiterals(formula, literals, occurrences, purity):
+    for i in range(0, len(occurrences)):
+        pure = purity[i]
+        if occurrences[i] == abs(pure):
+            literals[i] = int(pure/abs(pure))
+            formula.append(array('i', [literals[i] * i]))
+
+def dpll(formula, literals, occurrences):
+    clearUnits(formula, literals)
+    if not formula:
+        return literals, True
+    elif array('i') in formula:
+        return literals, False
+
+    maxOccurLiteral = literals.index(max(literals))
+    newLiterals = array('i', literals)
+    newFormula = deepcopy(formula)
+    newFormula.append(array('i', [maxOccurLiteral]))
+    (solution, satisfiable) = dpll(newFormula, newLiterals)
+    if satisfiable:
+        return solution, satisfiable
+    newLiterals = literals.index(max(literals))
+    newFormula = deepcopy(formula)
+    newFormula.append(array('i', [-maxOccurLiteral]))
+    return dpll(newFormula, newLiterals)
+
+
+def clearUnits(formula, literals):
+    i = 0
+    while i < len(formula):
+        clause = formula[i]
+        if len(clause) == 1:
+            literals[abs(clause[0]) - 1] = 1 if clause[0] > 0 else -1
+            j = 0
+            while j < len(formula):
+                next = False
+                tempClause = formula[j]
+                if set(tempClause) == set(clause):
+                    formula.remove(tempClause)
+                    next = True
+                elif clause[0] in tempClause:
+                    formula.remove(tempClause)
+                    next = True
+                elif -clause[0] in tempClause:
+                    tempClause.remove(-clause[0])
+                    next = True
+                j = j if next else j + 1
+            i = 0
+            continue
+        i += 1
+
+
+solve('dimacs/sudoku2.txt', 'temp/sudoku2_lol_izi.txt')
