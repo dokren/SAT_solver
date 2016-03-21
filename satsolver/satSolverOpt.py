a970930cce92b00cabf253a3028cebddf79a3d8b
rokkolesa <rok.kolesa@gmail.com>
2016-03-21 11:55:15 +0100

    Iterative version of dpll algorithm

--- satsolver/satSolverOpt.py
+++ satsolver/satSolverOpt.py
@@ -27,17 +27,43 @@ def dpll(formula, literals, occurrences):
     elif array('i') in formula:
         return literals, False
 
-    maxOccurLiteral = literals.index(max(literals))
-    newLiterals = array('i', literals)
-    newFormula = deepcopy(formula)
-    newFormula.append(array('i', [maxOccurLiteral]))
-    (solution, satisfiable) = dpll(newFormula, newLiterals, occurrences)
-    if satisfiable:
-        return solution, satisfiable
-    newLiterals = literals.index(max(literals))
-    newFormula = deepcopy(formula)
-    newFormula.append(array('i', [-maxOccurLiteral]))
-    return dpll(newFormula, newLiterals, occurrences)
+    level = 0
+    levels = dict()
+    i = literals.index(0)
+    levels[level] = list([formula, literals, i+1])
+    while True:
+        print('LEVEL ' + str(level))
+        print ('[', end="")
+        for (k, v) in levels.items():
+            print(str(v[2]) + ",", end="")
+        print (']')
+        (cFormula, cLiterals, cLit) = levels[level]
+        
+        newLiterals = array('i', cLiterals)
+        newFormula = deepcopy(cFormula)
+        if cLit > 0:
+            newFormula.append(array('i', [cLit]))
+        else:
+            newFormula.append(array('i', [-cLit]))
+            
+        clearUnits(newFormula, newLiterals)
+        if not newFormula:
+            return literals, True
+        elif array('i') in newFormula:
+            if cLit > 0:
+                levels[level] = list([cFormula, cLiterals, -cLit])
+            else:
+                if level == 0:
+                    return literals, False
+                else:
+                    level -= 1
+                    while levels[level][2] < 0:
+                        level -= 1
+                    levels[level][2] = -levels[level][2]
+        else:
+            level += 1
+            i = newLiterals.index(0)
+            levels[level] = list([newFormula, newLiterals, i+1])
 
 
 def clearUnits(formula, literals):
@@ -64,4 +90,4 @@ def clearUnits(formula, literals):
             continue
         i += 1
 
-solve('dimacs/test_case_not_satisfiable.cnf', 'temp/bf0432-007_lol_izi.cnf')
+solve('dimacs/sudoku2.txt', 'temp/test2_sol.txt')
